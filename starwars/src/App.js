a170d1d1f5647e681dfd4f8700c90d4f9eb9e6dd
BuildTools <arthurpisakhov1@gmail.com>
2019-09-07 16:41:14 -0400

    first data rendered

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,13 +11,28 @@ const App = () => {
   // sync up with, if any.
 
   const [name, setName] = useState('');
+  const [height, setHeight] = useState('');
+  const [mass, setMass] = useState('');
+  const [hair, setHair] = useState('');
+  const [skin, setSkin] = useState('');
+  const [eye, setEye] = useState('');
+  const [dob, setDob] = useState('');
+  const [gender, setGender] = useState('');
 
   useEffect(() => {
     // const fetchSwapi = () => 
       axios.get(`https://swapi.co/api/people/1`)
-        .then(res =>
+        .then(res => {
+          console.log(res.data)
           setName(res.data.name)
-        )
+          setHeight(res.data.height)
+          setMass(res.data.mass)
+          setHair(res.data.hair_color)
+          setSkin(res.data.skin_color)
+          setEye(res.data.eye_color)
+          setDob(res.data.birth_year)
+          setGender(res.data.gender)
+        })
   }, [])
 
   return (
@@ -25,6 +40,13 @@ const App = () => {
       <h1 className="Header">React Wars</h1>
       <div className="data-cont">
         <h2>{name}</h2>
+        <h3>{height}</h3>
+        <h3>{mass}</h3>
+        <h3>{hair}</h3>
+        <h3>{skin}</h3>
+        <h3>{eye}</h3>
+        <h3>{dob}</h3>
+        <h3>{gender}</h3>
       </div>
     </div>
   );
